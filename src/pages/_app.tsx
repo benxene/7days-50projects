d446597cc2ad645f566cb22bfad527e034d0d703
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-12-02 19:17:30 +0530

    added prettier format script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
-import Head from "next/head";
-import type { AppProps } from "next/app";
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // TODO: Write Global styles
   return (
     <>
       <Head>
-        <link
-          rel="icon"
-          href="favicon.png"
-          sizes="16x16 28x28 32x32"
-          type="image/png"
-        />
+        <link rel='icon' href='favicon.png' sizes='16x16 28x28 32x32' type='image/png' />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   // TODO: Create a nice Landing Page
