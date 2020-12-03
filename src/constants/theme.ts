d64ea7e418f43e691f4d96d548b746ea64b93dd1
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-12-03 16:41:40 +0530

    completed mobile tab navigation (#3)

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,6 +2,7 @@ export const colors = {
   primary: '#2d6cdf',
   secondary: '#fc5c9c',
   darkSlate: '#1a1a1a',
+  lightSlate: '#ccc',
   contrast: {
     primary: '#fff',
     darkSlate: '#2d6cdf'
@@ -9,6 +10,6 @@ export const colors = {
 };
 
 export const sizes = {
-  radius: '.6rem',
+  radius: { small: '.6rem', medium: '1rem', large: '2rem' },
   shadows: { light: '0 .2rem .3rem', big: '0 1rem 1.4rem' }
 };
