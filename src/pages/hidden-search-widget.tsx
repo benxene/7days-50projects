d16ae5bfb91ee7fedf1debafcd219857bae49601
PranavGPR <pranav.gnanasekar23@gmail.com>
2020-12-04 06:45:06 +0530

    base file setup

---
+++ src/pages/hidden-search-widget.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function HiddenWidget() {
+  return (
+    <div>
+      <Head>
+        <title>Hidden Search Widget | 7 days 50 projects</title>
+      </Head>
+      <h1>Hello!</h1>
+    </div>
+  );
+}
