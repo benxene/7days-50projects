3e8111960c9734112ad58373f3fbe9874c31b403
PranavGPR <pranav.gnanasekar23@gmail.com>
2020-12-04 23:55:24 +0530

    conflict fixed

--- src/pages/blurry-loading.tsx
+++ src/pages/blurry-loading.tsx
@@ -22,17 +22,10 @@ export default function BlurryLoading() {
         <title>Blurry Loading | 7 days 50 projects</title>
       </Head>
       <Container>
-<<<<<<< HEAD
         <TextContainer scale={calcIntensity(load, 0, 100, 30, 0)} load={load}>
           {' '}
         </TextContainer>
         <Text scale={calcIntensity(load, 0, 100, 1, 0)} load={load}>
-=======
-        <TextContainer scale={scale} load={load}>
-          {' '}
-        </TextContainer>
-        <Text scale={scale} load={load}>
->>>>>>> 8f13a4d4a5a171f6e2828cf2dcd7cf79e729d9e6
           {load}%
         </Text>
       </Container>
@@ -40,11 +33,7 @@ export default function BlurryLoading() {
   );
 }
 
-<<<<<<< HEAD
 const calcIntensity = (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => {
-=======
-const scale = (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => {
->>>>>>> 8f13a4d4a5a171f6e2828cf2dcd7cf79e729d9e6
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
@@ -55,24 +44,14 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-<<<<<<< HEAD
 const TextContainer = styled.div<{ scale: number; load: number }>`
   background: url('blurry-load-img.jpg') no-repeat center center/cover;
   filter: blur(${props => props.scale}px);
-=======
-const TextContainer = styled.div<{
-  scale: (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => number;
-  load: number;
-}>`
-  background: url('blurry-load-img.jpg') no-repeat center center/cover;
-  filter: blur(${props => Number(props.scale(props.load, 0, 100, 30, 0))}px);
->>>>>>> 8f13a4d4a5a171f6e2828cf2dcd7cf79e729d9e6
   position: absolute;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-<<<<<<< HEAD
 `;
 
 const Text = styled.div<{ scale: number; load: number }>`
@@ -80,16 +59,4 @@ const Text = styled.div<{ scale: number; load: number }>`
   font-size: 4rem;
   font-weight: 800;
   opacity: ${props => props.scale};
-=======
-  width: 100vw;
-  z-index: -1;
-`;
-
-const Text = styled.div<{ scale: any; load: number }>`
-  color: ${colors.contrast.primary};
-  font-size: 4rem;
-  font-weight: 800;
-  opacity: ${props => props.scale(props.load, 0, 100, 1, 0)};
-  /* z-index: 1000; */
->>>>>>> 8f13a4d4a5a171f6e2828cf2dcd7cf79e729d9e6
 `;
