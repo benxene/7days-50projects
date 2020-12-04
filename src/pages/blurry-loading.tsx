d76b988294f46bd3cec352dbddc627fb56567ade
PranavGPR <pranav.gnanasekar23@gmail.com>
2020-12-04 23:42:53 +0530

    small fixes

--- src/pages/blurry-loading.tsx
+++ src/pages/blurry-loading.tsx
@@ -22,10 +22,10 @@ export default function BlurryLoading() {
         <title>Blurry Loading | 7 days 50 projects</title>
       </Head>
       <Container>
-        <TextContainer scale={scale} load={load}>
+        <TextContainer scale={calcIntensity(load, 0, 100, 30, 0)} load={load}>
           {' '}
         </TextContainer>
-        <Text scale={scale} load={load}>
+        <Text scale={calcIntensity(load, 0, 100, 1, 0)} load={load}>
           {load}%
         </Text>
       </Container>
@@ -33,7 +33,7 @@ export default function BlurryLoading() {
   );
 }
 
-const scale = (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => {
+const calcIntensity = (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
@@ -44,25 +44,19 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const TextContainer = styled.div<{
-  scale: (num: number, in_min: number, in_max: number, out_min: number, out_max: number) => number;
-  load: number;
-}>`
+const TextContainer = styled.div<{ scale: number; load: number }>`
   background: url('blurry-load-img.jpg') no-repeat center center/cover;
-  filter: blur(${props => Number(props.scale(props.load, 0, 100, 30, 0))}px);
+  filter: blur(${props => props.scale}px);
   position: absolute;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  width: 100vw;
-  z-index: -1;
 `;
 
-const Text = styled.div<{ scale: any; load: number }>`
+const Text = styled.div<{ scale: number; load: number }>`
   color: ${colors.contrast.primary};
   font-size: 4rem;
   font-weight: 800;
-  opacity: ${props => props.scale(props.load, 0, 100, 1, 0)};
-  /* z-index: 1000; */
+  opacity: ${props => props.scale};
 `;
