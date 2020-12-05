d536b64a559ef8fa58581717c9cc08e878dbf5b0
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-12-05 22:55:19 +0530

    completed netflix mobile nav

--- src/pages/netflix-nav.tsx
+++ src/pages/netflix-nav.tsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styled from 'styled-components';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { GrClose } from 'react-icons/gr';
+import { GrFormClose } from 'react-icons/gr';
 import { CenterContainer } from '../components/Utilities';
 import { colors } from '../constants/theme';
 
 export default function NetflixNavigation() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const toggleVisibility = () => {
+    setOpen(prev => !prev);
+  };
+
   return (
     <>
       <Head>
         <title>Netflix Navigation | 7 days 50 projects</title>
       </Head>
       <CenterContainer color='#fff' style={{ textTransform: 'uppercase' }}>
-        <HamMenuButton>
+        <HamMenuButton onClick={toggleVisibility}>
           <GiHamburgerMenu />
         </HamMenuButton>
         <Image
@@ -24,33 +31,33 @@ export default function NetflixNavigation() {
           height='auto'
         />
         <p>Mobile Navigation</p>
-        <NavBar>
-          <NavContent>
-            <NavHeader>
-              <img
-                src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
-                loading='lazy'
-                width='150'
-                height='auto'
-              />
-              <CloseButton>
-                <GrClose />
-              </CloseButton>
-            </NavHeader>
-            <ul>
-              <li>Teams</li>
-              <li>Locations</li>
-              <li>LIFE AT NETFLIX</li>
-              <li>
-                <ul>
-                  <li>NETFLIX CULTURE MEMO </li>
-                  <li>WORK LIFE BALANCE </li>
-                  <li>INCLUSION & DIVERSITY BLOG </li>
-                </ul>
-              </li>
-            </ul>
-          </NavContent>
-        </NavBar>
+
+        <NavContent visible={isOpen}>
+          <NavHeader>
+            <img
+              src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
+              loading='lazy'
+              width='150'
+              height='auto'
+            />
+            <CloseButton onClick={toggleVisibility}>
+              <GrFormClose />
+            </CloseButton>
+          </NavHeader>
+          <ul>
+            <li>Teams</li>
+            <li>Locations</li>
+            <li>Life at Netflix</li>
+            <li>
+              <ul>
+                <li>Netflix culture memo</li>
+                <li>Work Life Balance </li>
+                <li>Inclusion & Diversity Blog </li>
+              </ul>
+            </li>
+          </ul>
+        </NavContent>
+        <NavBarBgs visible={isOpen} />
       </CenterContainer>
     </>
   );
@@ -74,21 +81,29 @@ const HamMenuButton = styled(IconButton)`
 `;
 
 const CloseButton = styled(IconButton)`
-  padding: 2rem;
+  font-size: 3rem;
+  padding: 1rem;
   color: ${colors.darkSlate};
   align-self: center;
 `;
 
-const NavBar = styled.div`
-  &::after {
+const NavBarBgs = styled.div<{ visible: boolean }>`
+  &::after,
+  &::before {
     content: '';
     position: fixed;
     top: 0;
     left: 0;
     height: 100vh;
+    transform: ${props => (props.visible ? 'translateX(0)' : 'translateX(-100%)')};
+    transition: all 0.3s ease-in-out;
+  }
+
+  &::after {
     width: 36rem;
     background-color: rgb(229, 9, 20);
     z-index: 100;
+    transition-delay: 0.2s;
 
     @media (max-width: 36rem) {
       width: calc(100% - 2rem);
@@ -96,14 +111,10 @@ const NavBar = styled.div`
   }
 
   &::before {
-    content: '';
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
     width: 38rem;
     background-color: rgb(34, 31, 31);
     z-index: 10;
+    transition-delay: ${props => (props.visible ? 0 : '.4s')};
 
     @media (max-width: 36rem) {
       width: 100%;
@@ -117,7 +128,7 @@ const NavHeader = styled.header`
   justify-content: space-between;
 `;
 
-const NavContent = styled.nav`
+const NavContent = styled.nav<{ visible: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
@@ -129,8 +140,12 @@ const NavContent = styled.nav`
   flex-direction: column;
   padding: 3rem;
   z-index: 1000;
+  transform: ${props => (props.visible ? 'translateX(0)' : 'translateX(-100%)')};
+  transition: all 0.3s ease-in-out;
+  transition-delay: ${props => (props.visible ? '.4s' : 0)};
 
   @media (max-width: 36rem) {
+    min-width: unset;
     width: calc(100% - 4rem);
   }
 
