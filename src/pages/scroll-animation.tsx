7603999b6a024fd7619532f19e5a7970b49aba39
sank2000 <santhoshvelr@yahoo.com>
2020-12-09 18:16:32 +0530

    docnment replaced by ref

--- src/pages/scroll-animation.tsx
+++ src/pages/scroll-animation.tsx
@@ -1,26 +1,11 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { colors } from '../constants/theme';
 
-const contents = [
-  'Content1',
-  'Content2',
-  'Content3',
-  'Content4',
-  'Content5',
-  'Content6',
-  'Content7',
-  'Content8',
-  'Content9',
-  'Content10',
-  'Content11',
-  'Content12'
-];
-
 const BoxComponent = () => {
   const [scroll, setScroll] = useState<boolean[]>([]);
-  const [boxes, setBoxes] = useState<NodeListOf<HTMLDivElement>>();
+  const boxes = useRef<any>([]);
 
   useEffect(() => {
     if (boxes === undefined) {
@@ -28,7 +13,7 @@ const BoxComponent = () => {
     } else {
       window.addEventListener('scroll', () => {
         const triggerBottom = (window.innerHeight / 5) * 4;
-        const scrollValue = [...boxes].map(box => {
+        const scrollValue = boxes.current.map((box: any) => {
           const boxHeight = box.getBoundingClientRect().top;
           return boxHeight < triggerBottom;
         });
@@ -36,18 +21,19 @@ const BoxComponent = () => {
       });
       window.scrollTo(0, 12);
     }
-  }, [boxes]);
-
-  useEffect(() => {
-    setBoxes(document.querySelectorAll('.box'));
   }, []);
 
   return (
     <>
-      {contents.map((content, index) => {
+      {[...new Array(12)].map((_, index) => {
         return (
-          <Box className='box' key={index} style={scroll[index] ? { transform: 'translateX(0)' } : {}}>
-            <h1>{content}</h1>
+          <Box
+            className='box'
+            ref={el => (boxes.current[index] = el)}
+            key={index}
+            style={scroll[index] ? { transform: 'translateX(0)' } : {}}
+          >
+            <h1>{`Content ${index + 1}`}</h1>
           </Box>
         );
       })}
