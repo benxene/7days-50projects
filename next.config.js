72da410a66ce668ab3ba507f9c933bf48e335c6a
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-12-10 19:26:13 +0530

    updates to index page (#115)
    * updated index page, listing projects and links
    
    * fixed project number starting from 0
    
    * added types, removed unused params & imports
    
    * screenshots added
    
    * rename *.JPG -> *.jpg & updated index config
    
    * changes made in image filename
    
    * added few screenshots
    
    * added contributors
    
    * added footer
    
    * final commit
    
    * fix warning: client-server link variation
    
    * rename *.JPG -> *.jpg
    
    Co-authored-by: Lakshmi <lakshmilatha342@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   images: {
-    domains: ['source.unsplash.com', 'upload.wikimedia.org', 'image.tmdb.org']
+    domains: [
+      'source.unsplash.com',
+      'upload.wikimedia.org',
+      'avatars0.githubusercontent.com',
+      'avatars1.githubusercontent.com',
+      'avatars3.githubusercontent.com',
+      'avatars2.githubusercontent.com',
+      'raw.githubusercontent.com', 
+      'image.tmdb.org'
+    ],
+    dangerouslyAllowAllDomains: true,
+    fallback: true
   }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import styled from 'styled-components';
+import axios from 'axios';
+import fs from 'fs';
 
-import Nav from '../components/NavigationBar';
-import { Container, Heading, Section } from '../components/Utilities';
-import Project from '../components/Project';
 import { colors } from '../constants/theme';
+import { Container, Heading, Section } from '../components/Utilities';
+import Nav from '../components/NavigationBar';
+import DevCard from '../components/DevCard';
+import Project from '../components/Project';
+import Footer from '../components/Footer';
 
-export default function Home() {
+export default function Home({ apps, team }: IProps) {
   return (
     <>
       <Head>
@@ -21,12 +26,18 @@ export default function Home() {
         <Container>
           <Heading center>Index</Heading>
           <ProjectsGrid>
-            <Project day={1} image='index.png' link='#'>
-              Index page
-            </Project>
-            <Project day={1} image='index.png' link='#'>
-              Index page
-            </Project>
+            {apps.map((app: { file: string; name: string }, num: number) => {
+              return (
+                <Project
+                  day={num + 1}
+                  key={app.file}
+                  image={`screenshots/${app.file}.jpg`}
+                  link={`/${app.file}`}
+                >
+                  {app.name}
+                </Project>
+              );
+            })}
           </ProjectsGrid>
         </Container>
       </Section>
@@ -34,10 +45,62 @@ export default function Home() {
         <Container>
           <Heading center>Our Team</Heading>
         </Container>
+        <TeamGrid>
+          {team.map(contributor => {
+            return (
+              !(contributor.login === 'imgbot[bot]') && (
+                <DevCard
+                  key={contributor.login}
+                  avatar={contributor.avatar_url}
+                  name={contributor.login}
+                  contributions={contributor.contributions}
+                  profile={`https://github.com/${contributor.login}`}
+                />
+              )
+            );
+          })}
+        </TeamGrid>
       </Section>
+      <Footer />
     </>
   );
 }
+
+interface IProps {
+  apps: Array<{ name: string; file: string }>;
+  team: Array<{
+    login: string;
+    avatar_url: string;
+    contributions: number;
+  }>;
+}
+
+export const getServerSideProps: GetServerSideProps = async _ => {
+  let files = fs.readdirSync('./src/pages');
+  files = files.map(file => {
+    return file.replace('.tsx', '');
+  });
+
+  files.splice(files.indexOf('_app'), 1);
+
+  const apps = files.map(file => {
+    let name = file.replace(/-/g, ' ');
+    if (name === 'index') name = 'Sticky nav';
+    name = name.charAt(0).toUpperCase() + name.substr(1);
+    return { name, file };
+  });
+
+  const team = (
+    await axios.get('https://api.github.com/repos/benxene/7days-50projects/contributors')
+  ).data;
+
+  return {
+    props: {
+      apps,
+      team
+    }
+  };
+};
 
 const Hero = styled.div`
   height: 100vh;
@@ -54,5 +117,22 @@ const ProjectsGrid = styled.div`
   place-content: center;
   grid-template-columns: repeat(auto-fill, 25rem);
   grid-auto-rows: minmax(18rem, 1fr);
-  grid-gap: 1.6rem;
+  grid-gap: 2.6rem;
+`;
+
+const TeamGrid = styled.div`
+  display: flex;
+  max-width: 90%;
+  margin: 1rem auto;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  & > *:not(:last-child) {
+    margin-right: 1.5rem;
+  }
+
+  & > * {
+    margin-bottom: 1.5rem;
+  }
 `;
