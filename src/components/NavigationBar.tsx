b86b23f71d4431aa5ab065b61efb239b7d605b3c
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-12-12 16:00:44 +0530

    improved navbar styles & links

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -18,13 +18,15 @@ export default function NavigationBar() {
 
   return (
     <Nav isScrolled={isScrolled}>
-      <Image
-        src='https://raw.githubusercontent.com/benxene/blobs/main/benxene-wo-name.png'
-        width='40'
-        height='40'
-        objectFit='contain'
-      />
-      <a href='https://github.com/benxene'>GitHub</a>
+      <a href='https://github.com/benxene' className='brand'>
+        <Image
+          src='https://raw.githubusercontent.com/benxene/blobs/main/benxene-wo-name.png'
+          width='40'
+          height='40'
+          objectFit='contain'
+        />
+      </a>
+      <a href='https://github.com/benxene/7days-50projects'>GitHub Repository</a>
     </Nav>
   );
 }
@@ -35,28 +37,31 @@ const Nav = styled.nav<{ isScrolled: boolean }>`
   left: 0;
   width: 100vw;
   padding: ${props => (props.isScrolled ? '1rem' : '2rem')} 6rem;
-  background-color: ${({ isScrolled }) => (isScrolled ? colors.contrast.primary : colors.primary)};
+  background-color: ${({ isScrolled }) => (isScrolled ? 'rgba(255,255,255,.75)' : 'transparent')};
+  backdrop-filter: ${({ isScrolled }) => (isScrolled ? 'blur(1rem)' : 'none')};
   box-shadow: ${props => (props.isScrolled ? '0 3px .5rem rgba(0,0,0,.2)' : 'none')};
   display: flex;
   align-items: center;
   transition: all 0.3s;
   z-index: 100;
 
-  & > *:not(:first-child) {
-    display: inline-block;
-    margin-right: 1rem;
+  & > * {
+    display: inline-flex;
+    align-items: center;
   }
 
   img {
     filter: ${({ isScrolled }) => (isScrolled ? 'initial' : 'brightness(100)')};
   }
 
+  .brand {
+    margin-right: auto;
+  }
+
   & > a {
     color: ${({ isScrolled }) => (isScrolled ? colors.secondary : colors.contrast.primary)};
     font-weight: 800;
     transition: color 0.2s;
-    display: inline-block;
-    margin-left: auto;
 
     &:hover {
       color: ${({ isScrolled }) => (isScrolled ? colors.primary : colors.secondary)};
