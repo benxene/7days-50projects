da32bab48e316999a8a33f348557e2eb52c6fe80
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2021-08-13 13:16:21 +0000

    removed unused Image imports

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styled from 'styled-components';
 import { colors, sizes } from '../constants/theme';
 
--- src/pages/skeleton.tsx
+++ src/pages/skeleton.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import styled, { keyframes } from 'styled-components';
 
 import { CenterContainer } from '../components/Utilities';
